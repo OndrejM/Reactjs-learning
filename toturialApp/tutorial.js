a590170b9f4da97049af79a30a8a02e719caea58
OndrejM <ondrej.mihalyi@gmail.com>
2015-07-29 10:52:28 +0000

    Added comments server in PHP (copied from tutorial github page). Fetching data from this server.

--- toturialApp/tutorial.js
+++ toturialApp/tutorial.js
@@ -1,30 +1,45 @@
 // tutorial1.js
 
-(function (React, marked) {
-    
-    var data = [
-      {author: "Pete Hunt", text: "This is one comment"},
-      {author: "Jordan Walke", text: "This is *another* comment"},
-      {author: "Ondrej", text: "This is my comment"}
-    ];
+/*
+  @param React from ReactJS
+  @param marked from marked
+  @param ajax Ajax helper object, contains method ajax.request, which has same semantics as $.ajax in jquery
+*/
+(function (React, marked, Ajax) {
     
     var c = components();
 
     React.render(
-      <c.CommentBox data={data}/>,
+      <c.CommentBox url="server/comments.php"/>,
       document.getElementById('content')
     );
 
     function components() {
         var c = {
-            /*  @prop data: Object - model
+            /*  @prop url: String - url of data service endpoint
             */
             CommentBox: React.createClass({
+                getInitialState: function() {
+                    return {data: []};
+                  },
+                componentDidMount: function() {
+                    Ajax.request({
+                      url: this.props.url,
+                      dataType: 'json',
+                      cache: false,
+                      success: function(data) {
+                        this.setState({data: data});
+                      }.bind(this),
+                      error: function(xhr, status, err) {
+                        console.error(this.props.url, status, err.toString());
+                      }.bind(this)
+                    });
+                  },                
                 render: function() {
                     return (
                         <div className="commentBox">
                             <h1>Comments</h1>
-                            <c.CommentList data={this.props.data}/>
+                            <c.CommentList data={this.state.data}/>
                             <c.CommentForm />
                         </div>
                     );
@@ -35,16 +50,13 @@
             */
             CommentList: React.createClass({
                 render: function() {
-                    var data = this.props.data;
                     return (
                       <div className="commentList">
-                        {data.map(function(comment) {
-                            return (
-                                <c.Comment author={comment.author}>
-                                    {comment.text}
-                                </c.Comment>
-                            );
-                        })}
+                        {this.props.data.map(comment => 
+                            <c.Comment author={comment.author}>
+                                {comment.text}
+                            </c.Comment>
+                        )}
                       </div>
                     );
                     
@@ -82,8 +94,5 @@
         return c;
     }
 
-/*
-  @param React from ReactJS
-  @param marked from marked
-*/
-})(React, marked);
+})(React, marked, 
+    {request: $.ajax});
